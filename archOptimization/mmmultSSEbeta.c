9515b35da1e3e4360f8047c4bae4ba7bd7a81468
Henry Nicholas Riordan <hriordan@slowpoke.cs.uchicago.edu>
2013-12-03 07:39:56 -0600

    SSE-mmult, for real this time

--- archOptimization/mmmultSSEbeta.c
+++ archOptimization/mmmultSSEbeta.c
@@ -16,7 +16,6 @@
 #define YOURSIZE 100    // define your own
 
 
-//Working out SSE instrinsics 
  
 //thank you, guy rutenberg
 struct timespec diff(struct timespec start, struct timespec end)
@@ -31,19 +30,7 @@ struct timespec diff(struct timespec start, struct timespec end)
 	}
 	return temp;
 }
-/*
-void temp_first(int *temp, int* matrix, int d, int size) { //to create temporary contiguous array of first[k][d] to be turned into m128
 
-	int i;
-	
-	for (i=0; i<size; i++)
-		temp[i] = matrix[i*size+d];
-	
-	//return ret; 
-	
-} 
-
-*/
 
 int main(int argc, char  **argv)
 {
@@ -75,15 +62,12 @@ int main(int argc, char  **argv)
   posix_memalign((void**)&first,16,sizeof(int)*size*size);  //use posix_memalign to get 16byte alignment 
   int *multiply; 
   posix_memalign((void**)&multiply,16,sizeof(int)*size*size);  
-  //int *temp;
-  //posix_memalign((void**)&temp, 16, sizeof(int)*size);
   __m128i m1, m2,m3; 
  
-/* Matrix generation proceeds normally? */  
   for (  c = 0 ; c < size ; c++ )
   	for ( d = 0 ; d < size ; d++ )    	
       first[c*size+d] = ((c+d) % 2) - 1;
-	
+  multiply[c*size+d] = 0;
       
 	
   printf("multiplying the %d-size matrices\n  You should try to time this part.\n",size);
@@ -95,13 +79,13 @@ int main(int argc, char  **argv)
 		for ( k = 0 ; k < size ; k++ )
 	    {
 	    	m2 = _mm_set1_epi32(first[c*size+k]); //first[c][k]
-			for (d = 0 ; d < size ; d+=4)  //this inner loop can be SSEed
+			for (d = 0 ; d < size ; d+=4) 
 			{
 				edge = size - d; 
 				if (edge < 4){	//account for non-div by 4 matrices
 					for (i = d; i < size; i++)
 						multiply[c*size+i] += first[c*size+k]*first[k*size+i];
-					}
+				}
 				else{ 
 				  m1 = _mm_loadu_si128(&first[k*size+d]);  //first[k][d]
 				  m1 = _mm_mullo_epi32(m1,m2); // first[k][d] * first[c][k]
@@ -127,10 +111,10 @@ int main(int argc, char  **argv)
   
   fclose(fp);
 
-  printf("blah %d\n",first[size]);
-  printf("blah %d\n",multiply[size]);
+  printf("test first %d\n",first[size]);
+  printf("test mult %d\n",multiply[size]);
   
-//free(temp);
+
   free(first);   //free SSE aligned array with _aligned_free
   free(multiply);
   return 0;
